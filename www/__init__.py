b7e3d0fef9a5ca821edd97aae8efee28a076cccd
max <maxe.ludwig@googlemail.com>
2012-02-15 03:07:29 +0100

    finishing integration of Flask-Admin for Page and Post (no User yet) + protecting it with user auth

--- www/__init__.py
+++ www/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, redirect, request, url_for
 www = Flask(__name__)
 www.config.from_pyfile('../config.cfg')
 
-from flaskext.login import LoginManager, login_required
+from flaskext.login import LoginManager, current_user
 login = LoginManager()
 login.setup_app(www, add_context_processor=True)
 login.login_view = 'login'
@@ -15,8 +15,8 @@ mail = Mail(www)
 
 from www import tweaks
 from www import main
-from www.user import User
-from www.page import page, Page
+from www import user
+from www.page import page, Page, PageForm
 www.register_blueprint(page)
 from www.blog import blog, Post, PostForm
 www.register_blueprint(blog, url_prefix='/blog')
@@ -25,6 +25,12 @@ www.register_blueprint(contact, url_prefix='/contact')
 
 from flask.ext import admin
 from flask.ext.admin.datastore.sqlalchemy import SQLAlchemyDatastore
-admin_datastore = SQLAlchemyDatastore((User, Page, Post), db.session, model_forms={'Post': PostForm})
+admin_datastore = SQLAlchemyDatastore((Page, Post), db.session, model_forms={'Post': PostForm, 'Page': PageForm})
 admin_blueprint = admin.create_admin_blueprint(admin_datastore)
-www.register_blueprint(admin_blueprint, url_prefix='/admin', view_decorator=login_required)
+
+def check_auth_for_admin():
+	if not current_user.is_authenticated():
+		return redirect(url_for('login', next=request.url))
+
+admin_blueprint.before_request(check_auth_for_admin)
+www.register_blueprint(admin_blueprint, url_prefix='/admin')
